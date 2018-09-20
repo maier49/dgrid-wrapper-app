440664875698ffcf54f5bbfae75b07ff1297a02e
Bradley Maier <bmaier@sitepen.com>
2018-09-20 16:56:41 -0400

    Working tests

--- testLoaderConfig.js
+++ testLoaderConfig.js
@@ -1,11 +1,11 @@
 const globalObj = typeof window !== 'undefined' ? window : global;
 globalObj.dojoConfig = {
-    baseUrl: "output/test/externals",
+    baseUrl: '../../output/test/unit',
     packages: [
-        { name: 'dojo', location: 'dojo' },
-        { name: 'dijit', location: 'dijit' },
-        { name: 'dgrid', location: 'dgrid' },
-        { name: 'dstore', location: 'dstore' }
+        { name: 'dojo', location: 'externals/dojo' },
+        { name: 'dijit', location: 'externals/dijit' },
+        { name: 'dgrid', location: 'externals/dgrid' },
+        { name: 'dstore', location: 'externals/dstore' }
     ],
     has: {
         highcontrast: 0
